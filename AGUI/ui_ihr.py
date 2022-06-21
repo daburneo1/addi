3c8db68a1bd6fac36d18ea66d64deddbcb9a1458
davichov2013 <davichov2013@gmail.com>
2022-06-21 10:25:01 -0500

    add-threads-in-ui

--- AGUI/ui_ihr.py
+++ AGUI/ui_ihr.py
@@ -1,4 +1,8 @@
 import threading
+import time
+
+from PyQt5.QtCore import QTimer, QDateTime
+from PyQt5.QtGui import QMovie
 
 from BLOGICA.LOGIhr import *
 
@@ -20,16 +24,18 @@ class Ihr_Form(QWidget):
 
         self.pushButtonAcceso.clicked.connect(self.acceso)
         self.pushButtonPosponer.clicked.connect(self.posponer)
-        self.pushButtonConfirmar.clicked.connect(self.confirmar)
+        self.pushButtonConfirmar.clicked.connect(self.stop)
         self.pushButtonCancelar.clicked.connect(self.cancelar)
 
     def ejecucion_horaria(self):
         log_ihr = LOGIhr
-        while(True):
+        self._go = True
+        while(self._go):
             print(f"[{time.ctime()}] >$ ", 'start')
             recordatorios = log_ihr.consultar_db()
             if recordatorios:
                 self.ejecutar_recordatorio(recordatorios)
+                print('OK')
                 seconds = self.calcular_espera()
                 time.sleep(seconds)
             else:
@@ -43,29 +49,67 @@ class Ihr_Form(QWidget):
         self.ui_login.show()
         self.close()
 
+    # def ejecutar_recordatorio(self, recordatorios):
+    #     self._go = True
+    #     s = "mustnotchange"
+    #     while (self._go):
+    #         print('Bucle')
+    #         s = self.input_with_timeout(s, 5, recordatorios[0])
+    #         print(s)
+    #         if s == "mustnotchange":
+    #             print(f"\n[{time.ctime()}] Recordatorio en 5 minutos.")
+    #             time.sleep(5)
+    #         else:
+    #             print(s)
+    #             print("Gracias, hasta la próxima")
+    #             break
+
     def ejecutar_recordatorio(self, recordatorios):
-        s = "mustnotchange"
-        while (True):
-            s = self.input_with_timeout(s, 5, recordatorios[0])
-            print(s)
-            if s == "mustnotchange":
-                print(f"\n[{time.ctime()}] Recordatorio en 5 minutos.")
-                time.sleep(5)
-            else:
-                print(s)
-                print("Gracias, hasta la próxima")
-                break
+        medicine = recordatorios[0]
+        self._go = True
+        while (self._go):
+            print('Bucle')
+            print('Hola, te recuerdo que debes tomar en este momento ' + medicine.nombre)
+            self.movie = QMovie("../Iconos/giphy.gif")
+            self.Emoji.setMovie(self.movie)
+            self.movie.start()
+            # time.sleep(5)
+            # if s == "mustnotchange":
+            #     print(f"\n[{time.ctime()}] Recordatorio en 5 minutos.")
+            #     time.sleep(5)
+            # else:
+            #     print(s)
+            #     print("Gracias, hasta la próxima")
+            #     break
 
     def input_with_timeout(self, default, timeout, medicine):
         print('Hola, te recuerdo que debes tomar en este momento ' + medicine.nombre)
         print('Presiona Enter para confirmar')
         pm = PromptManager(timeout)
         ans = pm.start()
-        self.pushButtonConfirmar.clicked.connect(self.confirmar)
         if isinstance(ans, str):
             return ans
         else:
             return default
+
+    # def input_with_timeout(self, default, timeout, medicine):
+    #     pm = threading.Thread(target=self.recordatorio(timeout, medicine))
+    #     ans = pm.start()
+    #     return ans
+
+    def recordatorio(self, timeout, medicine):
+        a = True
+        while a:
+            self.pushButtonConfirmar.clicked.connect(self.stop)
+            # if ans == 'ok':
+            #     break
+
+            # try:
+            #     ans = inputimeout(f"[{time.ctime()}] >$ ", timeout=5)
+            # except TimeoutOccurred:
+            #     ans = ''
+            # if ans != '':
+            #     return 'ok'
 
     def calcular_espera(self):
         hora_actual = datetime.today()
@@ -75,11 +119,14 @@ class Ihr_Form(QWidget):
         print(seconds)
         return seconds
 
+    def stop(self):
+        self._go = False
+
     def posponer(self):
         pass
 
     def confirmar(self):
-        print('CONFIRMAR')
+        pass
 
     def cancelar(self):
         pass

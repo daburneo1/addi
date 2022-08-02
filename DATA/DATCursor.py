4c755505d2897fb8a7830d1c3eaf4c282e58591a
daburneo1 <davichov2013@gmail.com>
2022-08-02 00:08:16 -0500

    update

--- DATA/DATCursor.py
+++ DATA/DATCursor.py
@@ -1,16 +1,16 @@
 import pymysql
 
-connection = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",
-    db="addi"
-)
 # connection = pymysql.connect(
 #     host="localhost",
-#     user="addi",
-#     password="addi",
+#     user="root",
+#     password="",
 #     db="addi"
 # )
+connection = pymysql.connect(
+    host="localhost",
+    user="addi",
+    password="addi",
+    db="addi"
+)
 
 cursor = connection.cursor()

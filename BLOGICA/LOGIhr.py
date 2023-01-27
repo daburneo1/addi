cc438d061c86c6b170940cad52521a18e2b9f300
daburneo1 <davichov2013@gmail.com>
2023-01-26 22:30:57 -0500

    update

--- BLOGICA/LOGIhr.py
+++ BLOGICA/LOGIhr.py
@@ -114,45 +114,21 @@ class LOGIhr():
         import board
         import neopixel
 
-        pixel_pin = board.D18
-        pixel_pin2 = board.D12
+        pixel_pin = board.D10
 
         num_pixels = 9
 
         ORDER = neopixel.GRB
-        a = 0
+
         pixels = neopixel.NeoPixel(
             pixel_pin, num_pixels, brightness = 0.2, auto_write = False, pixel_order = ORDER
         )
-
-        def wheel(self, pos):
-            if pos < 0 or pos > 255:
-                r = g = b = 0
-
-            elif pos < 85:
-                r = int(pos * 3)
-                g = int(255 - pos * 3)
-                b = 0
-
-            elif pos < 170:
-                pos -= 85
-                r = int(255 - pos * 3)
-                g = 0
-                b = int(pos * 3)
-
-            else:
-                pos -= 170
-                r = 0
-                g = int(pos * 3)
-                b = int(255 - pos * 3)
-            return(r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
         if estado == 1:
             pixels.fill((0, 255, 0))
             pixels.show()
             print('verde')
-            time.sleep(5)
-        elif estado == 2 or estado == 4:
+        elif estado == 2:
             pixels.fill((0, 0, 0))
             pixels.show()
             print('sin luces')
@@ -161,7 +137,7 @@ class LOGIhr():
             pixels.fill((0, 0, 255))
             pixels.show()
             print('azul')
-            time.sleep(5)
+        else:
             pixels.fill((0, 0, 0))
             pixels.show()
             print('sin luces')
